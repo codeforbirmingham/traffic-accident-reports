87bdd390caa74ae3d78bd422f34352c9ba5e4aa2
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-02-21 15:52:56 -0600

    Fix SoQL query encoding.

--- scripts/traffic-accidents.js
+++ scripts/traffic-accidents.js
@@ -6,9 +6,17 @@
     endpoint = "https://brigades.opendatanetwork.com/resource/caz5-9y2j.json";
     token = "iqxU70pZzU1Bo0swgGR4Ad93m";
 
-    req = function (queryString, callback) {
+    req = function (query, callback) {
         var urlParams;
-        urlParams = "?" + encodeURIComponent(queryString);
+        urlParams = "";
+        Object.keys(query).forEach(function (clause, index) {
+            if (index === 0) {
+                urlParams = urlParams + "?";
+            } else {
+                urlParams = urlParams + "&"
+            }
+            urlParams = urlParams + clause + "=" + encodeURIComponent(query[clause]);
+        });
         jQuery.ajax({
             url: endpoint + urlParams,
             headers: {
@@ -23,7 +31,7 @@
 
     window["TrafficAccidents"] =  {
         getAllRows: function (callback) {
-            req("", callback);
+            req({}, callback);
         }
     };
 
