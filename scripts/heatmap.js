d050097797eb304ee8b9333aa2d1b87948dd83ed
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-02-22 13:18:00 -0600

    Add redraw function.

--- scripts/heatmap.js
+++ scripts/heatmap.js
@@ -1,47 +1,48 @@
 (function () {
     "use strict";
 
+    var baseLayer, heatmapLayer, map, redraw;
+
+    baseLayer = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
+        attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+        subdomains: '1234',
+        mapID: 'newest',
+        app_id: 'oBrjPtnB5CZNkvOVmmqw',
+        app_code: 'TPBtO9fTES--LhkVo5sKeA',
+        base: 'base',
+        minZoom: 0,
+        maxZoom: 20
+    });
+
+    heatmapLayer = new HeatmapOverlay({});
+
+    map = L.map("heatmap", {
+        center: L.latLng(33.5250, -86.8130),
+        zoom: 12,
+        minZoom: 12,
+        layers: [baseLayer, heatmapLayer]
+    });
+
+    redraw = function () {
+        var heatmapData;
+        heatmapData = TrafficAccidents.groupByLocation().map(function (location) {
+            return {
+                lat: location.value.lat,
+                lng: location.value.lng,
+                value: location.key,
+                radius: location.value.count
+            };
+        });
+        heatmapLayer.setData({
+            data: heatmapData
+        });
+    };
+
     TrafficAccidents.fetch(function () {
-
-        var baseLayer, heatmapLayer, map, heatmapData;
-
      // Hide loader and show map.
         $("#loader").hide();
-        $("#mainframe").show();
-
-        baseLayer = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
-            attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
-            subdomains: '1234',
-            mapID: 'newest',
-            app_id: 'oBrjPtnB5CZNkvOVmmqw',
-            app_code: 'TPBtO9fTES--LhkVo5sKeA',
-            base: 'base',
-            minZoom: 0,
-            maxZoom: 20
-        });
-
-        heatmapLayer = new HeatmapOverlay({});
-
-        map = L.map("heatmap", {
-            center: L.latLng(33.5250, -86.8130),
-            zoom: 12,
-            minZoom: 12,
-            layers: [baseLayer, heatmapLayer]
-        });
-
-        heatmapData = {
-            data: TrafficAccidents.groupByLocation().map(function (location) {
-                return {
-                    lat: location.value.lat,
-                    lng: location.value.lng,
-                    value: location.key,
-                    radius: location.value.count
-                };
-            })
-        };
-
-        heatmapLayer.setData(heatmapData);
-
+        $("#mainframe").css("visibility", "visible");
+        redraw();
     });
 
 }());
